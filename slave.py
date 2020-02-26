88a1aa5f97050677c400cfdf4ee38818fa1c583c
root <root@localhost.localdomain>
2020-02-26 00:49:41 -0500

    first commit

---
+++ slave.py
@@ -0,0 +1,28 @@
+import os
+import threading
+import subprocess
+
+def recv():
+    while 1:
+        res = subprocess.check_output(["head","/dev/ttyS0","-n","1"])
+        if res!=b"" and res!=b'\n':
+            recv = res.decode("utf-8")[:-1]
+            date = subprocess.check_output(["date","+%Y-%m-%d"]).decode("utf-8")[:-1]
+            time = subprocess.check_output(["date","+%H:%M:%S"]).decode("utf-8")[:-1]
+            print("[RECV",date,time+"]",recv)
+
+def send():
+    while 1:
+        input_str = input()
+        if(input_str!=""):
+            os.system("echo "+input_str+" >> /dev/ttyS1")
+            date = subprocess.check_output(["date","+%Y-%m-%d"]).decode("utf-8")[:-1]
+            time = subprocess.check_output(["date","+%H:%M:%S"]).decode("utf-8")[:-1]
+            print("[SEND",date,time+"]",input_str)
+
+S = threading.Thread(target=send, name='sendThread')
+R = threading.Thread(target=recv, name='recvThread')
+S.start()
+R.start()
+S.join()
+R.join()
